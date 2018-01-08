c2eb615b06d28ea905aea272a7cbb8af3526f612
涂晓风 <1660921696@qq.com>
2018-01-08 13:55:11 +0800

    test

--- jasset/views.py
+++ jasset/views.py
@@ -452,6 +452,16 @@ def asset_option(request):
     return my_render('jasset/asset_option.html', locals(), request)
 
 @require_role('admin')
+def asset_set(request):
+    """
+    Asset add view
+    服务设置
+    """
+    header_title, path1, path2 = u'服务设置', u'资产管理', u'服务设置'
+
+    return my_render('jasset/asset_set.html', locals(), request)
+
+@require_role('admin')
 def asset_detail(request):
     """
     Asset detail view
